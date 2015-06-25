31316f708bddb964582af03144844083009cea5f
elainapolson <elainapolson@gmail.com>
2015-06-25 12:23:15 -0400

    cleaned up a bit

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,12 +1,7 @@
-#to_slug module
-
-
-  module Slugifiable 
-    module ClassMethods
-    end
-    module InstanceMethods
-      def to_slug
-        self.name.downcase.gsub(" ", "-")
-      end 
-    end
+module Slugifiable 
+  module InstanceMethods
+    def to_slug
+      self.name.downcase.gsub(" ", "-")
+    end 
   end
+end
